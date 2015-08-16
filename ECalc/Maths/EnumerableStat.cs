2064f553a06ab14c7df01e2805d92348323ae985
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2015-08-16 17:54:46 +0200

    Stat Update & NumberText code

--- ECalc/Maths/EnumerableStat.cs
+++ ECalc/Maths/EnumerableStat.cs
@@ -6,8 +6,37 @@ using System.Threading.Tasks;
 
 namespace ECalc.Maths
 {
+    /// <summary>
+    /// Stat functions for enumerable sets. Portions taken from:
+    /// https://github.com/mariusschulz/NAverage/blob/master/NAverage and
+    /// http://www.codeproject.com/Articles/42492/Using-LINQ-to-Calculate-Basic-Statistics
+    /// </summary>
     internal static class EnumerableStat
     {
+
+        /// <summary>
+        /// Contains the exception that is thrown when ApproximationIterations null collection argument is passed.
+        /// </summary>
+        private static readonly ArgumentNullException ArgumentNullException =
+            new ArgumentNullException("numbers", "The collection must not be null!");
+
+        /// <summary>
+        /// Contains the exception that is thrown when an empty collection is passed.
+        /// </summary>
+        private static readonly InvalidOperationException EmptyNumbersCollectionException
+            = new InvalidOperationException("The collection must not be empty!");
+
+        /// <summary>
+        /// Contains the exception that is thrown when ApproximationIterations collection containing negative numbers is passed.
+        /// </summary>
+        private static readonly InvalidOperationException CollectionContainingNegativeNumbersException =
+            new InvalidOperationException("The collection must not contain negative numbers!");
+
+        /// <summary>
+        /// Variance is the measure of the amount of variation of all the scores for a variable (not just the extremes which give the range).
+        /// </summary>
+        /// <param name="source">The numbers whose variation is to be calculated.</param>
+        /// <returns>The variation of the given numbers.</returns>
         public static double Variance(IEnumerable<double> source)
         {
             int n = 0;
@@ -24,11 +53,22 @@ namespace ECalc.Maths
             return M2 / (n - 1);
         }
 
+        /// <summary>
+        /// The Standard Deviation of a statistical population, a data set, or a probability distribution is the square root of its variance.
+        /// </summary>
+        /// <param name="source">The numbers whose Standard Deviation is to be calculated.</param>
+        /// <returns>The  Standard Deviation of the given numbers.</returns>
         public static double StandardDeviation(IEnumerable<double> source)
         {
             return Math.Sqrt(Variance(source));
         }
 
+
+        /// <summary>
+        /// Calculates the median of the given numbers.
+        /// </summary>
+        /// <param name="source">The numbers whose median is to be calculated.</param>
+        /// <returns>The median of the given numbers.</returns>
         public static double Median(IEnumerable<double> source)
         {
             var sortedList = from number in source
@@ -45,29 +85,171 @@ namespace ECalc.Maths
             return sortedList.ElementAt(itemIndex);
         }
 
+        /// <summary>
+        /// Mode is the value that occurs the most frequently in a data set or a probability distribution.
+        /// </summary>
+        /// <param name="source">The numbers whose mode is to be calculated.</param>
+        /// <returns>The mode of the given numbers.</returns>
+        public static double Mode(this IEnumerable<double> source)
+        {
+            var sortedList = from number in source
+                             orderby number
+                             select number;
+
+            int count = 0;
+            int max = 0;
+            double current = 0.0;
+            double mode = 0.0;
+
+            foreach (double next in sortedList)
+            {
+                if (current.Equals(next) == false)
+                {
+                    current = next;
+                    count = 1;
+                }
+                else count++;
+
+                if (count > max)
+                {
+                    max = count;
+                    mode = current;
+                }
+            }
+
+            if (max > 1) return mode;
+
+            return double.NaN;
+        }
+
+        /// <summary>
+        /// Range is the length of the smallest interval which contains all the data.
+        /// </summary>
+        /// <param name="source">The numbers whose range is to be calculated.</param>
+        /// <returns>The range of the given numbers.</returns>
         public static double Range(IEnumerable<double> source)
         {
             return source.Max() - source.Min();
         }
 
+        /// <summary>
+        /// Counts the items in the set
+        /// </summary>
+        /// <param name="source">The numbers whose count is to be calculated.</param>
+        ///  <returns>The count of the given numbers.</returns>
         public static double Count(IEnumerable<double> source)
         {
             return source.Count();
         }
 
+        /// <summary>
+        /// Returns the minumum value of the set
+        /// </summary>
+        /// <param name="source">The numbers whose minimum is to be calculated.</param>
+        /// <returns>The minimum of the given numbers.</returns>
         public static double Minimum(IEnumerable<double> source)
         {
+            if (!source.Any()) throw EmptyNumbersCollectionException;
             return source.Min();
         }
 
+        /// <summary>
+        /// Returns the maximum value of the set
+        /// </summary>
+        /// <param name="source">The numbers whose maximum is to be calculated.</param>
+        /// <returns>The maximum of the given numbers.</returns>
         public static double Maximum(IEnumerable<double> source)
         {
+            if (!source.Any()) throw EmptyNumbersCollectionException;
             return source.Max();
         }
 
-        public static double Average(IEnumerable<double> source)
+        /// <summary>
+        /// Calculates the arithmetic mean of the given numbers.
+        /// </summary>
+        /// <param name="source">The numbers whose arithmetic mean is to be calculated.</param>
+        /// <returns>The arithmetic mean of the given numbers.</returns>
+        public static double ArithmeticMean(IEnumerable<double> source)
         {
+            if (!source.Any()) throw EmptyNumbersCollectionException;
             return source.Average();
+        }
+
+        /// <summary>
+        /// Calculates the geometric mean of the given numbers.
+        /// </summary>
+        /// <param name="source">The numbers whose geometric mean is to be calculated.</param>
+        /// <returns>The geometric mean of the given numbers.</returns>
+        public static double GeometricMean(IEnumerable<double> source)
+        {
+            if (!source.Any()) throw EmptyNumbersCollectionException;
+
+            if (source.Any(number => number < 0)) throw CollectionContainingNegativeNumbersException;
+            double productOfNumbers = 1;
+
+            foreach (double number in source)
+            {
+                productOfNumbers *= number;
+            }
+
+            double numbersCount = source.Count();
+            double exponent = 1.0 / numbersCount;
+            double geometricMean = Math.Pow(productOfNumbers, exponent);
+
+            return geometricMean;
+        }
+
+        /// <summary>
+        /// Calculates the harmonic mean of the given numbers.
+        /// The harmonic mean is defined as zero if at least one of the numbers is zero.
+        /// </summary>
+        /// <param name="source">The numbers whose harmonic mean is to be calculated.</param>
+        /// <returns>The harmonic mean of the given numbers.</returns>
+        public static double HarmonicMean(IEnumerable<double> source)
+        {
+            if (!source.Any()) throw EmptyNumbersCollectionException;
+
+            if (source.Any(number => number < 0)) throw CollectionContainingNegativeNumbersException;
+
+            if (source.Contains(0)) return 0;
+
+            double sumOfReciprocalValues = source.Sum(number => 1.0 / number);
+            double harmonicMean = source.Count() / sumOfReciprocalValues;
+
+            return harmonicMean;
+        }
+
+        /// <summary>
+        /// Calculates the midrange of the specified collection of numbers.
+        /// </summary>
+        /// <param name="source">The numbers whose midrange is to be calculated.</param>
+        /// <returns>The midrange of the specified collection of numbers.</returns>
+        public static double Midrange(IEnumerable<double> source)
+        {
+            if (!source.Any()) throw EmptyNumbersCollectionException;
+
+            double minValue = source.Min();
+            double maxValue = source.Max();
+
+            double midrange = (minValue + maxValue) / 2.0d;
+
+            return midrange;
+        }
+
+        /// <summary>
+        /// Calculates the quadratic mean (or RMS, respectively) of the specified numbers.
+        /// </summary>
+        /// <param name="source">The numbers whose quadratic mean is to be calculated.</param>
+        public static double QuadraticMean(IEnumerable<double> source)
+        {
+            if (!source.Any()) throw EmptyNumbersCollectionException;
+
+            IEnumerable<double> squaredNumbers = source.Select(n => n * n);
+
+            double arithmeticMeanOfSquaredNumbers = ArithmeticMean(squaredNumbers);
+            double quadraticMean = Math.Sqrt(arithmeticMeanOfSquaredNumbers);
+
+            return quadraticMean;
         }
     }
 }
---
+++ ECalc/Maths/NumberText.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECalc.Maths
+{
+    /// <summary>
+    /// Based on: https://github.com/robertgreiner/NumberText/blob/master/NumberText/NumberText.cs
+    /// </summary>
+    internal class NumberText
+    {
+
+        private Dictionary<int, string> textStrings = new Dictionary<int, string>();
+        private Dictionary<int, string> scales = new Dictionary<int, string>();
+        private StringBuilder builder;
+
+        public NumberText()
+        {
+            Initialize();
+        }
+
+        /// <summary>
+        /// Convert a number to text
+        /// </summary>
+        /// <param name="num">number to convert</param>
+        /// <returns>returns a string representaion of the number</returns>
+        public string ToText(int num)
+        {
+            builder = new StringBuilder();
+
+            if (num == 0)
+            {
+                builder.Append(textStrings[num]);
+                return builder.ToString();
+            }
+
+            num = scales.Aggregate(num, (current, scale) => Append(current, scale.Key));
+            AppendLessThanOneThousand(num);
+
+            return builder.ToString().Trim();
+        }
+
+        private int Append(int num, int scale)
+        {
+            if (num > scale - 1)
+            {
+                var baseScale = ((int)(num / scale));
+                AppendLessThanOneThousand(baseScale);
+                builder.AppendFormat("{0} ", scales[scale]);
+                num = num - (baseScale * scale);
+            }
+            return num;
+        }
+
+        private int AppendLessThanOneThousand(int num)
+        {
+            num = AppendHundreds(num);
+            num = AppendTens(num);
+            AppendUnits(num);
+            return num;
+        }
+
+        private void AppendUnits(int num)
+        {
+            if (num > 0)
+            {
+                builder.AppendFormat("{0} ", textStrings[num]);
+            }
+        }
+
+        private int AppendTens(int num)
+        {
+            if (num > 20)
+            {
+                var tens = ((int)(num / 10)) * 10;
+                builder.AppendFormat("{0} ", textStrings[tens]);
+                num = num - tens;
+            }
+            return num;
+        }
+
+        private int AppendHundreds(int num)
+        {
+            if (num > 99)
+            {
+                var hundreds = ((int)(num / 100));
+                builder.AppendFormat("{0} hundred ", textStrings[hundreds]);
+                num = num - (hundreds * 100);
+            }
+            return num;
+        }
+
+        private void Initialize()
+        {
+            textStrings.Add(0, "zero");
+            textStrings.Add(1, "one");
+            textStrings.Add(2, "two");
+            textStrings.Add(3, "three");
+            textStrings.Add(4, "four");
+            textStrings.Add(5, "five");
+            textStrings.Add(6, "six");
+            textStrings.Add(7, "seven");
+            textStrings.Add(8, "eight");
+            textStrings.Add(9, "nine");
+            textStrings.Add(10, "ten");
+            textStrings.Add(11, "eleven");
+            textStrings.Add(12, "twelve");
+            textStrings.Add(13, "thirteen");
+            textStrings.Add(14, "fourteen");
+            textStrings.Add(15, "fifteen");
+            textStrings.Add(16, "sixteen");
+            textStrings.Add(17, "seventeen");
+            textStrings.Add(18, "eighteen");
+            textStrings.Add(19, "nineteen");
+            textStrings.Add(20, "twenty");
+            textStrings.Add(30, "thirty");
+            textStrings.Add(40, "forty");
+            textStrings.Add(50, "fifty");
+            textStrings.Add(60, "sixty");
+            textStrings.Add(70, "seventy");
+            textStrings.Add(80, "eighty");
+            textStrings.Add(90, "ninety");
+            textStrings.Add(100, "hundred");
+
+            scales.Add(1000000000, "billion");
+            scales.Add(1000000, "million");
+            scales.Add(1000, "thousand");
+        }
+    }
+}
