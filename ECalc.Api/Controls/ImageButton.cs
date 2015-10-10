76ef3327b000e18e59c6c4d79f0df28b4b75a116
webmaster442 <ruzsinszki.gabor@gmail.com>
2015-10-10 11:33:58 +0200

    Rework to Plugin API subsystem

---
+++ ECalc.Api/Controls/ImageButton.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace ECalc.Api.Controls
+{
+    /// <summary>
+    /// ImageButton Control
+    /// </summary>
+    public class ImageButton: Button
+    {
+        static ImageButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ImageButton), new FrameworkPropertyMetadata(typeof(ImageButton)));
+        }
+
+        public static readonly DependencyProperty ImageProperty = DependencyProperty.Register("Image", typeof(ImageSource), typeof(ImageButton), new PropertyMetadata(null));
+
+        public static readonly DependencyProperty ImageHeightProperty =  DependencyProperty.Register("ImageHeight", typeof(double), typeof(ImageButton), new PropertyMetadata(Double.NaN));
+
+        public static readonly DependencyProperty ImageWidthProperty = DependencyProperty.Register("ImageWidth", typeof(double), typeof(ImageButton), new PropertyMetadata(Double.NaN));
+
+        /// <summary>
+        /// Gets or sets the control image
+        /// </summary>
+        public ImageSource Image
+        {
+            get { return (ImageSource)GetValue(ImageProperty); }
+            set { SetValue(ImageProperty, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets the image heiht
+        /// </summary>
+        public double ImageHeight
+        {
+            get { return (double)GetValue(ImageHeightProperty); }
+            set { SetValue(ImageHeightProperty, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets the image width
+        /// </summary>
+        public double ImageWidth
+        {
+            get { return (double)GetValue(ImageWidthProperty); }
+            set { SetValue(ImageWidthProperty, value); }
+        }
+        
+    }
+
+    public class VisibilityConvertor : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value == null)
+                return Visibility.Collapsed;
+            return Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
---
+++ ECalc.Api/EcalcModule.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace ECalc.Api
+{
+    /// <summary>
+    /// Engineer Calculator module
+    /// </summary>
+    public abstract class EcalcModule
+    {
+        /// <summary>
+        /// Module name
+        /// </summary>
+        public abstract string  ModuleName { get; }
+
+        /// <summary>
+        /// Module category
+        /// </summary>
+        public abstract string ModuleCategory { get; }
+
+        /// <summary>
+        /// Get the Control of the module
+        /// </summary>
+        /// <returns>The module UserControl</returns>
+        public abstract UserControl GetControl();
+
+        /// <summary>
+        /// Control tile color
+        /// </summary>
+        public virtual SolidColorBrush Color
+        {
+            get { return new SolidColorBrush(Colors.YellowGreen); }
+        }
+
+        /// <summary>
+        /// Control tile icon
+        /// </summary>
+        public virtual BitmapImage Icon
+        {
+            get { return null; }
+        }
+    }
+}
