ae31adc0be7221db17d76fe5b6ec78d517058d6e
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2015-11-23 15:45:03 +0100

    Prefix dictionary bug fix

--- ECalc/Classes/PrefixDictionary.cs
+++ ECalc/Classes/PrefixDictionary.cs
@@ -7,20 +7,21 @@ namespace ECalc.Classes
     /// <summary>
     /// Dictionary override with prefixes
     /// </summary>
-    internal class PrefixDictionary: Dictionary<string, double>
+    internal class PrefixDictionary : Dictionary<string, double>
     {
-        public PrefixDictionary(): base(19)
+        public PrefixDictionary() : base(19)
         {
-            this.Add("da", Math.Pow(10, 1));
             this.Add("y", Math.Pow(10, -24));
             this.Add("z", Math.Pow(10, -21));
-            this.Add("f", Math.Pow(10, -18));
+            this.Add("a", Math.Pow(10, -18));
+            this.Add("f", Math.Pow(10, -15));
+            this.Add("p", Math.Pow(10, -12));
             this.Add("n", Math.Pow(10, -9));
             this.Add("u", Math.Pow(10, -6));
             this.Add("m", Math.Pow(10, -3));
             this.Add("c", Math.Pow(10, -2));
             this.Add("d", Math.Pow(10, -1));
-            this.Add("a", Math.Pow(10, -18));
+            this.Add("da", Math.Pow(10, 1));
             this.Add("h", Math.Pow(10, 2));
             this.Add("k", Math.Pow(10, 3));
             this.Add("M", Math.Pow(10, 6));
@@ -45,7 +46,7 @@ namespace ECalc.Classes
             var sorted = from i in this where i.Value > 999 || i.Value <= 0.001 orderby i.Value descending select i;
             foreach (var prefix in sorted)
             {
-                if (final > prefix.Value)
+                if (final >= prefix.Value)
                 {
                     final /= prefix.Value;
                     text = prefix.Key;
