4bf9289ea510de706779b9b22d7eaeed4489649f
webmaster442 <ruzsinszki.gabor@gmail.com>
2015-11-24 10:40:40 +0100

    DateTimeSelector control

---
+++ ECalc/Controls/Special/DateTimeSelector.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ECalc.Controls.Special
+{
+    /// <summary>
+    /// Interaction logic for DateTimeSelector.xaml
+    /// </summary>
+    public partial class DateTimeSelector : UserControl
+    {
+        public DateTimeSelector()
+        {
+            InitializeComponent();
+        }
+
+        public DateTime SelectedDateTime
+        {
+            get
+            {
+                DateTime dt = (DateTime)DatePicker.SelectedDate;
+                return dt.Add(new TimeSpan((int)SelH.Value, (int)SelM.Value, (int)SelS.Value));
+            }
+            set
+            {
+                DateTime dt = value;
+                DatePicker.SelectedDate = dt;
+                SelH.Value = dt.Hour;
+                SelM.Value = dt.Minute;
+                SelS.Value = dt.Second;
+            }
+        }
+
+        private void BtnLocal_Click(object sender, RoutedEventArgs e)
+        {
+            SelectedDateTime = DateTime.Now;
+        }
+
+        private void BtnUTC_Click(object sender, RoutedEventArgs e)
+        {
+            SelectedDateTime = DateTime.UtcNow;
+        }
+    }
+}
