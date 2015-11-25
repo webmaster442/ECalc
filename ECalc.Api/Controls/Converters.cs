53c5ee347d0ecc8aa7a53931b17bfaa43e1eb006
webmaster442 <ruzsinszki.gabor@gmail.com>
2015-11-25 12:50:51 +0100

    Draft of improoved memory manager control

---
+++ ECalc.Api/Controls/Converters.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace ECalc.Api.Controls
+{
+    /// <summary>
+    /// Base class for converters to be used in the x: xaml namespace
+    /// </summary>
+    public abstract class BaseConverter : MarkupExtension
+    {
+        /// <summary>
+        /// returns an object that is provided as the value of the target property for this markup extension
+        /// </summary>
+        /// <param name="serviceProvider"> A service provider helper that can provide services for the markup extension.</param>
+        /// <returns>The object value to set on the property where the extension is applied.</returns>
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+
+    /// <summary>
+    /// Converts an object selection value to a bool.
+    /// Returns true, if the source object is not null
+    /// </summary>
+    [ValueConversion(typeof(object), typeof(bool))]
+    public class SelectedToEnabledConverter : BaseConverter, IValueConverter
+    {
+        /// <summary>
+        /// Converts a value.
+        /// </summary>
+        /// <param name="value">The value produced by the binding source.</param>
+        /// <param name="targetType">The type of the binding target property.</param>
+        /// <param name="parameter">The converter parameter to use.</param>
+        /// <param name="culture">The culture to use in the converter.</param>
+        /// <returns>A converted value. If the method returns null, the valid null value is used.</returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null) return true;
+            else return false;
+        }
+
+        /// <summary>
+        /// Converts a value. In this case it's not implemented
+        /// </summary>
+        /// <param name="value">The value that is produced by the binding target.</param>
+        /// <param name="targetType">The type to convert to.</param>
+        /// <param name="parameter">The converter parameter to use.</param>
+        /// <param name="culture">The culture to use in the converter.</param>
+        /// <returns>A converted value. If the method returns null, the valid null value is used.</returns>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ECalc/Controls/MemoryManagement.xaml.cs
@@ -0,0 +1,144 @@
+﻿using ECalc.Classes;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ECalc.Controls
+{
+    /// <summary>
+    /// Interaction logic for MemoryManagement.xaml
+    /// </summary>
+    public partial class MemoryManagement : UserControl, IMemManager
+    {
+        private ObservableCollection<MemoryItem> _memory;
+
+        public MemoryManagement()
+        {
+            InitializeComponent();
+            _memory = new ObservableCollection<MemoryItem>();
+        }
+
+        public event RoutedEventHandler CancelClicked;
+        public event StringEventHandler InsertClicked;
+        public event RoutedEventHandler FromExpressionClick;
+
+        #region IMemManager
+        /// <summary>
+        /// Gets the value of a register item
+        /// </summary>
+        /// <param name="name">item to get</param>
+        /// <returns>the value of the item</returns>
+        public object GetItem(string name)
+        {
+            if (name == "$ans") return Engine.Ans;
+            var query = from i in _memory where string.Compare(name, i.Name) == 0 select i;
+            var result = query.FirstOrDefault();
+            if (result == null) return null;
+            else return result.Value;
+        }
+
+        /// <summary>
+        /// Set an item with name
+        /// </summary>
+        /// <param name="name">name of variable</param>
+        /// <param name="value">vallue of variable</param>
+        public void SetItem(string name, object value)
+        {
+            if (name == "$ans") return;
+            var query = (from i in _memory where string.Compare(name, i.Name) == 0 select i).FirstOrDefault();
+            if (query == null) _memory.Add(new MemoryItem(name, value));
+            else
+            {
+                int index = _memory.IndexOf(query);
+                _memory[index].Value = value;
+            }
+        }
+
+        /// <summary>
+        /// Lists register names
+        /// </summary>
+        /// <param name="query">query string. If null or empty all registers will be returned</param>
+        /// <returns>An array of register names</returns>
+        public string[] ListRegisters(string query)
+        {
+            if (string.IsNullOrEmpty(query))
+            {
+                return (from i in _memory select i.Name).ToArray();
+            }
+            else
+            {
+                var q = from i in _memory where i.Name.StartsWith(query) select i.Name;
+                return q.ToArray();
+            }
+        }
+        #endregion
+
+        private void BtnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            if (CancelClicked != null)
+            {
+                CancelClicked(sender, e);
+            }
+        }
+
+        private void BtnFromLast_Click(object sender, RoutedEventArgs e)
+        {
+            _memory.Add(new MemoryItem(Engine.Ans));
+        }
+
+        private void BtnFromExpression_Click(object sender, RoutedEventArgs e)
+        {
+            if (FromExpressionClick != null)
+            {
+                FromExpressionClick(sender, e);
+            }
+        }
+
+        private void BtnInsert_Click(object sender, RoutedEventArgs e)
+        {
+            if (InsertClicked != null)
+            {
+                var content = _memory[MemList.SelectedIndex].Name;
+                InsertClicked(sender, new StringEventArgs(content));
+            }
+        }
+
+        private void BtnLoad_Click(object sender, RoutedEventArgs e)
+        { 
+            System.Windows.Forms.OpenFileDialog ofd = new System.Windows.Forms.OpenFileDialog();
+            ofd.Filter = "Memory Files | *.mem";
+            ofd.Multiselect = false;
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+
+            }
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.SaveFileDialog sfd = new System.Windows.Forms.SaveFileDialog();
+            sfd.Filter = "Memory Files | *.mem";
+            sfd.AddExtension = true;
+            if (sfd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+
+            }
+        }
+
+        private void BtnNew_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            _memory.RemoveAt(MemList.SelectedIndex);
+        }
+    }
+}
