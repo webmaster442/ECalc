e8ed66f3b3a7055485f55802ca87cb7e89141161
webmaster442 <ruzsinszki.gabor@gmail.com>
2015-12-07 10:08:38 +0100

    Assembly info update

--- ECalc.Docs/Properties/AssemblyInfo.cs
+++ ECalc.Docs/Properties/AssemblyInfo.cs
@@ -8,10 +8,10 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ECalc.Docs")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Engineering Calculator Documentation browser")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ECalc.Docs")]
+[assembly: AssemblyCompany("Webmaster442")]
+[assembly: AssemblyProduct("Engineering Calculator")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -53,3 +53,5 @@ using System.Windows;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
+
