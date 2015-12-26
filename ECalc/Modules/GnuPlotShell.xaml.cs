8d67cf836e7a925daafaae1c9b05fee06b574d49
webmaster442 <ruzsinszki.gabor@gmail.com>
2015-12-26 22:19:06 +0100

    GnuPlotShell initial code

--- ECalc/Modules/GnuPlotShell.xaml.cs
+++ ECalc/Modules/GnuPlotShell.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ECalc.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,11 +19,48 @@ namespace ECalc.Modules
     /// <summary>
     /// Interaction logic for GnuPlotShell.xaml
     /// </summary>
-    public partial class GnuPlotShell : UserControl
+    public partial class GnuPlotShell : UserControl, IDisposable
     {
+        private GnuPlotRunner _gnuplot;
+
         public GnuPlotShell()
         {
             InitializeComponent();
+        }
+
+        ~GnuPlotShell()
+        {
+            Dispose(true);
+        }
+
+        protected void Dispose(bool native)
+        {
+            if (_gnuplot != null)
+            {
+                _gnuplot.Kill();
+                _gnuplot.Dispose();
+                _gnuplot = null;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                _gnuplot = new GnuPlotRunner();
+                _gnuplot.Start();
+            }
+            catch (Exception ex)
+            {
+                TbOutput.Foreground = new SolidColorBrush(Colors.Red);
+                TbOutput.Text = "Error:\r\n" + ex.Message;
+            }
         }
     }
 }
