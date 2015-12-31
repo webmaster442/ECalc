397e89b88da1a699c7db377b4edb229bba569e4e
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2015-12-31 12:48:51 +0100

    Work on new Function list started

---
+++ ECalc/Controls/FunctionList2.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ECalc.Classes;
+
+namespace ECalc.Controls
+{
+    /// <summary>
+    /// Interaction logic for FunctionList2.xaml
+    /// </summary>
+    public partial class FunctionList2 : UserControl
+    {
+        public FunctionList2()
+        {
+            InitializeComponent();
+        }
+
+        internal IFunction Funtions
+        {
+            get;
+            set;
+        }
+    }
+}
