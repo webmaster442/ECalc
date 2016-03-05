276b185d0833fc87016a2f707236c1f1c4f4dc26
webmaster442 <ruzsinszki.gabor@gmail.com>
2016-03-05 21:36:30 +0100

    WindowManager completed

--- ECalc/Classes/WindowManager.cs
+++ ECalc/Classes/WindowManager.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using ECalc.Api.Extensions;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Windows;
+using System.Windows.Media;
 
 namespace ECalc.Classes
 {
@@ -36,6 +39,20 @@ namespace ECalc.Classes
         }
 
         /// <summary>
+        /// Closes all child windows
+        /// </summary>
+        public static void CloseAll()
+        {
+            while (_childs.Count > 0)
+            {
+                var child = _childs[0];
+                _childs.RemoveAt(0);
+                child.Close();
+                child = null;
+            }
+        }
+
+        /// <summary>
         /// Stops tracking of a child window
         /// </summary>
         /// <param name="w"></param>
@@ -66,6 +83,42 @@ namespace ECalc.Classes
             }
         }
 
+        /// <summary>
+        /// Returns a window based on it's index in the _childs collection
+        /// </summary>
+        /// <param name="index">index of the window</param>
+        /// <returns>a Window instance</returns>
+        public static Window GetWindow(int index)
+        {
+            return _childs[index];
+        }
 
+        /// <summary>
+        /// Renders the previews for each child window 
+        /// </summary>
+        public static ObservableCollection<WindowData> Previews
+        {
+            get
+            {
+                var ret = new ObservableCollection<WindowData>();
+
+                foreach (var window in _childs)
+                {
+                    ret.Add(new WindowData()
+                    {
+                        Image = window.Thumbnail(),
+                        Title = window.Title
+                    });
+                }
+
+                return ret;
+            }
+        }
+    }
+
+    internal class WindowData
+    {
+        public string Title { get; set; }
+        public ImageSource Image { get; set; }
     }
 }
--- ECalc/Pages/WindowManager.xaml.cs
+++ ECalc/Pages/WindowManager.xaml.cs
@@ -1,18 +1,7 @@
 ﻿using ECalc.Classes;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ECalc.Pages
 {
@@ -24,6 +13,55 @@ namespace ECalc.Pages
         public WindowsManager()
         {
             InitializeComponent();
+        }
+
+        private void BtnMinimizeAll_Click(object sender, RoutedEventArgs e)
+        {
+            WindowManager.MinimizeAll();
+        }
+
+        private void BtnRestoreAll_Click(object sender, RoutedEventArgs e)
+        {
+            WindowManager.RestoreAll();
+        }
+
+        private void BtnCloseAll_Click(object sender, RoutedEventArgs e)
+        {
+            WindowManager.CloseAll();
+            Refresh();
+        }
+
+        private void Refresh()
+        {
+            WinList.ItemsSource = null;
+            WinList.ItemsSource = WindowManager.Previews;
+        }
+
+        private void BtnRefresh_Click(object sender, RoutedEventArgs e)
+        {
+            Refresh();
+        }
+
+        private void WinBringToFront_Click(object sender, RoutedEventArgs e)
+        {
+            var index = WinList.SelectedIndex;
+            if (index < 0) return;
+            var window = WindowManager.GetWindow(index);
+            WindowManager.BringToFront(window);
+        }
+
+        private void WinClose_Click(object sender, RoutedEventArgs e)
+        {
+            var index = WinList.SelectedIndex;
+            if (index < 0) return;
+            try
+            {
+                var window = WindowManager.GetWindow(index);
+                WindowManager.UnRegisterChild(window);
+                window.Close();
+            }
+            catch (Exception) { }
+            Refresh();
         }
     }
 }
