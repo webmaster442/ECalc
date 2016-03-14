583b05ff7bc9c9cac495a49256634604c5371a9d
webmaster442 <ruzsinszki.gabor@gmail.com>
2016-03-14 19:54:57 +0100

    Added more TileColor options

---
+++ ECalc.Api/TileColor.cs
@@ -0,0 +1,181 @@
+﻿namespace ECalc.Api
+{
+    /// <summary>
+    /// Tile Color Enumeration
+    /// </summary>
+    public enum TileColor : int
+    {
+        /// <summary>
+        /// Default Gray color
+        /// </summary>
+        Default = 0xCCCCCC,
+        /// <summary>
+        /// Light Green
+        /// </summary>
+        W8LightGreen = 0x99b433,
+        /// <summary>
+        /// Lime
+        /// </summary>
+        W8Lime = 0xA4C400,
+        /// <summary>
+        /// Green
+        /// </summary>
+        W8Green = 0x60A917,
+        /// <summary>
+        /// Dark Green
+        /// </summary>
+        W8DarkGreen = 0x1e7145,
+        /// <summary>
+        /// Dark orange
+        /// </summary>
+        W8DarkOrange = 0xda532c,
+        /// <summary>
+        /// Emerald
+        /// </summary>
+        W8Emerald = 0x008A00,
+        /// <summary>
+        /// Teal
+        /// </summary>
+        W8Teal = 0x00ABA9,
+        /// <summary>
+        /// Cyan
+        /// </summary>
+        W8Cyan = 0x1BA1E2,
+        /// <summary>
+        /// Cobalt
+        /// </summary>
+        W8Cobalt = 0x0050EF,
+        /// <summary>
+        /// Indigo
+        /// </summary>
+        W8Indigo = 0x6A00FF,
+        /// <summary>
+        /// Violet
+        /// </summary>
+        W8Violet = 0xAA00FF,
+        /// <summary>
+        /// Pink
+        /// </summary>
+        W8Pink = 0xF472D0,
+        /// <summary>
+        /// Magenta
+        /// </summary>
+        W8Magenta = 0xD80073,
+        /// <summary>
+        /// Crimson
+        /// </summary>
+        W8Crimson = 0xA20025,
+        /// <summary>
+        /// Red
+        /// </summary>
+        W8Red = 0xE51400,
+        /// <summary>
+        /// Orange
+        /// </summary>
+        W8Orange = 0xFA6800,
+        /// <summary>
+        /// Amber
+        /// </summary>
+        W8GAmber = 0xF0A30A,
+        /// <summary>
+        /// Yellow
+        /// </summary>
+        W8Yellow = 0xE3C800,
+        /// <summary>
+        /// Brown
+        /// </summary>
+        W8Brown = 0x825A2C,
+        /// <summary>
+        /// Olive
+        /// </summary>
+        W8Olive = 0x6D8764,
+        /// <summary>
+        /// Steel
+        /// </summary>
+        W8Steel = 0x647687,
+        /// <summary>
+        /// Mauve
+        /// </summary>
+        W8Mauve = 0x76608A,
+        /// <summary>
+        /// Turquoise
+        /// </summary>
+        FlatTurquoise = 0x1abc9c,
+        /// <summary>
+        /// Emerald
+        /// </summary>
+        FlatEmerald = 0x2ecc71,
+        /// <summary>
+        /// Peter River
+        /// </summary>
+        FlatPeterRiver = 0x3498db,
+        /// <summary>
+        /// Amethyst
+        /// </summary>
+        FlatAmethyst = 0x9b59b6,
+        /// <summary>
+        /// Wet Asphalt
+        /// </summary>
+        FlatWetAsphalt = 0x34495e,
+        /// <summary>
+        /// Geen Sea
+        /// </summary>
+        FlatGeenSea = 0x16a085,
+        /// <summary>
+        /// Nephritis
+        /// </summary>
+        FlatNephritis = 0x27ae60,
+        /// <summary>
+        /// Belize Hole
+        /// </summary>
+        FlatBelizeHole = 0x2980b9,
+        /// <summary>
+        /// Wisteria
+        /// </summary>
+        FlatWisteria = 0x8e44ad,
+        /// <summary>
+        /// Midnight Blue
+        /// </summary>
+        FlatMidnightBlue = 0x2c3e50,
+        /// <summary>
+        /// Sun Flower
+        /// </summary>
+        FlatSunFlower = 0xf1c40f,
+        /// <summary>
+        /// Carrot
+        /// </summary>
+        FlatCarrot = 0xe67e22,
+        /// <summary>
+        /// Alizarin
+        /// </summary>
+        FlatAlizarin = 0xe74c3c,
+        /// <summary>
+        /// Clouds
+        /// </summary>
+        FlatClouds = 0xecf0f1,
+        /// <summary>
+        /// Concrete
+        /// </summary>
+        FlatConcrete = 0x95a5a6,
+        /// <summary>
+        /// Orange
+        /// </summary>
+        FlatOrange = 0xf39c12,
+        /// <summary>
+        /// Pumpkin
+        /// </summary>
+        FlatPumpkin = 0xd35400,
+        /// <summary>
+        /// Pomegrande
+        /// </summary>
+        FlatPomegrande = 0xc0392b,
+        /// <summary>
+        /// Silver
+        /// </summary>
+        FlatSilver = 0xbdc3c7,
+        /// <summary>
+        /// Asbestos
+        /// </summary>
+        FlatAsbestos = 0x7f8c8d,
+    }
+}
