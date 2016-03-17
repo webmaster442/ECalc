381ae477785b8a2bb0fe12fd3ed26a01bbdcc4e6
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2016-03-17 22:15:18 +0100

    User function base type

---
+++ ECalc/Classes/UserFuntions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+
+namespace ECalc.Classes
+{
+    [Serializable]
+    public class UserFuntion : IXmlSerializable
+    {
+        /// <summary>
+        /// User function name
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// User function argument count
+        /// </summary>
+        public int ArgCount { get; set; }
+        /// <summary>
+        /// User function run commands
+        /// </summary>
+        public string Commands { get; set; }
+
+        public XmlSchema GetSchema()
+        {
+            return null;
+        }
+
+        public void ReadXml(XmlReader reader)
+        {
+            Name = reader.GetAttribute("Name");
+            ArgCount = Convert.ToInt32(reader.GetAttribute("Arguments"));
+            Commands = reader.ReadElementContentAsString();
+        }
+
+        public void WriteXml(XmlWriter writer)
+        {
+            writer.WriteAttributeString("Name", Name);
+            writer.WriteAttributeString("Arguments", ArgCount.ToString());
+            writer.WriteElementString("Code", Commands);
+        }
+    }
+}
