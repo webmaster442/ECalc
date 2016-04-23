e1819ef5ea308e0c3760b0acec0f8cf3186ca0c4
webmaster442 <ruzsinszki.gabor@gmail.com>
2016-04-23 21:07:44 +0200

    Updated compile script

--- Compile.cmd
+++ Compile.cmd
@@ -10,12 +10,23 @@ if exist bin\release echo I | rmdir /s bin\release
 rem ---------------------------------------------------------------------------
 rem setup
 rem ---------------------------------------------------------------------------
-set msbuildp=%windir%\Microsoft.NET\Framework\v4.0.30319\
-SET PATH=%PATH%;%msbuildp%;
+if %processor_architecture%==AMD64 goto x64
+goto x86
+
+:x86
+call "%PROGRAMFILES%\Microsoft Visual Studio 14.0\VC\bin\vcvars32.bat"
+echo "32 bit system"
+goto compile
+
+:x64
+echo "64 bit system"
+call "%PROGRAMFILES(x86)%\Microsoft Visual Studio 14.0\VC\bin\vcvars32.bat"
+goto compile
 
 rem ---------------------------------------------------------------------------
 rem compile
 rem ---------------------------------------------------------------------------
+:compile
 echo Running MSBuild...
 msbuild /m ECalc.sln /p:Configuration=Release /l:FileLogger,Microsoft.Build.Engine;logfile=compile.log;append=false
 if %errorlevel%==1 echo Build Failed. Check compile.log
