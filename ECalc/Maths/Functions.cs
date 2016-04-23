66814469c53dc0afce97c32fc3b44ae4dade29e4
webmaster442 <ruzsinszki.gabor@gmail.com>
2016-04-23 19:39:17 +0200

    Variation functions added Optimized Fact function

--- ECalc/Maths/Functions.cs
+++ ECalc/Maths/Functions.cs
@@ -84,12 +84,44 @@ namespace ECalc.Maths
         [Category("General")]
         public static double Fact(double target)
         {
-            double result = 1;
-            for (int i = 1; i < target; i++)
+
+            switch ((long)target)
             {
-                result *= i;
+                case 0:
+                case 1:
+                    return 1;
+                case 2:
+                    return 2;
+                case 3:
+                    return 6;
+                case 4:
+                    return 24;
+                case 5:
+                    return 120;
+                case 6:
+                    return 720;
+                case 7:
+                    return 5040;
+                case 8:
+                    return 40320;
+                case 9:
+                    return 362880;
+                case 10:
+                    return 3628800;
+                case 11:
+                    return 39916800;
+                case 12:
+                    return 479001600;
+                case 13:
+                    return 6227020800;
+                default:
+                    double result = 6227020800;
+                    for (int i = 14; i <= target; i++)
+                    {
+                        result *= i;
+                    }
+                    return result;
             }
-            return result;
         }
        
         /// <summary>
---
+++ ECalc/Maths/Variations.cs
@@ -0,0 +1,51 @@
+﻿using ECalc.IronPythonEngine;
+using System;
+using System.Linq;
+
+namespace ECalc.Maths
+{
+    [Loadable]
+    public static class Variations
+    {
+        [Category("Variations")]
+        public static double VariationNoRepeat(double n, double k)
+        {
+            return Functions.Fact(n) / Functions.Fact(n - k);
+        }
+
+        [Category("Variations")]
+        public static double VariationRepeat(double n, double k)
+        {
+            return Math.Pow(n, k);
+        }
+
+        [Category("Variations")]
+        public static double CombinationNoRepeat(double n, double k)
+        {
+            return Functions.Fact(n) / Functions.Fact(n - k) * Functions.Fact(k);
+        }
+
+        [Category("Variations")]
+        public static double CombinationRepeat(double n, double k)
+        {
+            double n2 = (n + k) - 1;
+            return Functions.Fact(n2) / Functions.Fact(n2 - k) * Functions.Fact(k);
+        }
+
+        [Category("Variations")]
+        public static double Permutation(double n, params double[] repeatk)
+        {
+            double ktest = repeatk.Sum();
+            if (ktest > n)
+                throw new ArgumentException("Sum of repeating elements is bigger than the first parameter!", nameof(repeatk));
+
+            double factn = Functions.Fact(n);
+            double sk = 1;
+            foreach (var k in repeatk)
+            {
+                sk *= Functions.Fact(k);
+            }
+            return factn / sk;
+        }
+    }
+}
