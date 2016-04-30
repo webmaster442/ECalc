c130b69c3d288a28dd18706fed90bb48e3193537
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2016-04-30 14:22:15 +0200

    Added update button & update code

--- ECalc/Pages/Menu.xaml.cs
+++ ECalc/Pages/Menu.xaml.cs
@@ -2,6 +2,8 @@
 using System.Windows;
 using System.Windows.Controls;
 using ECalc.Docs;
+using System.Deployment.Application;
+using System;
 
 namespace ECalc.Pages
 {
@@ -56,6 +58,81 @@ namespace ECalc.Pages
         private void BtnChangeTheme_Click(object sender, RoutedEventArgs e)
         {
             App.NextTheme();
+        }
+
+        private void BtnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateCheckInfo info = null;
+
+            if (ApplicationDeployment.IsNetworkDeployed)
+            {
+                ApplicationDeployment ad = ApplicationDeployment.CurrentDeployment;
+                try
+                {
+                    info = ad.CheckForDetailedUpdate();
+                }
+                catch (DeploymentDownloadException dde)
+                {
+                    MessageBox.Show("The new version of the application cannot be downloaded at this time. \n\n"+
+                        "Please check your network connection, or try again later.\n"+
+                        "Error: " + dde.Message);
+                    return;
+                }
+                catch (InvalidDeploymentException ide)
+                {
+                    MessageBox.Show("Cannot check for a new version of the application.\n"+
+                        "The ClickOnce deployment is corrupt. Please redeploy the application and try again.\n"
+                        +"Error: " + ide.Message);
+                    return;
+                }
+                catch (InvalidOperationException ioe)
+                {
+                    MessageBox.Show("This application cannot be updated. It is likely not a ClickOnce application.\n"+
+                        "Error: " + ioe.Message);
+                    return;
+                }
+
+                if (info.UpdateAvailable)
+                {
+                    Boolean doUpdate = true;
+
+                    if (!info.IsUpdateRequired)
+                    {
+                        var dr = MessageBox.Show("An update is available. Would you like to update the application now?", "Update Available", MessageBoxButton.YesNoCancel);
+                        if (dr == MessageBoxResult.No) doUpdate = false;
+                    }
+                    else
+                    {
+                        // Display a message that the app MUST reboot. Display the minimum required version.
+                        MessageBox.Show("This application has detected a mandatory update from your current " +
+                            "version to version " + info.MinimumRequiredVersion.ToString() +
+                            ". The application will now install the update and restart.",
+                            "Update Available", MessageBoxButton.OK, MessageBoxImage.Information);
+                    }
+
+                    if (doUpdate)
+                    {
+                        try
+                        {
+                            ad.Update();
+                            MessageBox.Show("The application has been upgraded, and will now restart.");
+                            System.Diagnostics.Process.Start(Application.ResourceAssembly.Location);
+                            Application.Current.Shutdown();
+                        }
+                        catch (DeploymentDownloadException dde)
+                        {
+                            MessageBox.Show("Cannot install the latest version of the application. \n\n"+
+                                "Please check your network connection, or try again later. \n"+
+                                "Error: " + dde);
+                            return;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("This application cannot be updated. It is likely not a ClickOnce application.");
+            }
         }
     }
 }
